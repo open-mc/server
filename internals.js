d9c6925ea24bf7d24fa3f378333dea71594dac71
BlobTheKat <64211737+BlobTheKat@users.noreply.github.com>
2023-03-21 13:21:31 +0000

    Update internals.js

--- internals.js
+++ internals.js
@@ -3,12 +3,12 @@ import {promises as fs, exists} from 'fs'
 import { setFlagsFromString } from 'v8'
 import './utils/prototypes.js'
 import { runInThisContext } from 'vm'
-import './utils/prototypes.js'
+
 setFlagsFromString('--allow-natives-syntax')
 // TODO: research on %CompileOptimized_Concurrent
 export const optimize = new Function('...fns', 'for(const f of fns)%OptimizeFunctionOnNextCall(f)')
 
-globalThis.PATH = decodeURI(import.meta.url).replace(/[^\/]*(\.js)?$/,"").replace(/file:\/\/(\w+:\/)?/y,'')
+globalThis.PATH = decodeURI(import.meta.url).replace(/[^\/]*(\.js)?$/,"").replace(/file:\/\/(\/\w+:)?/y,'')
 globalThis.WORLD = PATH + '../' + (process.argv[2] || 'world') + '/'
 
 fs.exists = a => new Promise(r => exists(a, r))
