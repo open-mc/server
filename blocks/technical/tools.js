87a0da24e95be8c21ddb37f1f403a65499211ad3
BlobTheKat <mat.reiner@icloud.com>
2024-01-30 23:22:51 +0000

    a

--- blocks/technical/tools.js
+++ blocks/technical/tools.js
@@ -9,44 +9,46 @@ class CraftingInterface extends EphemeralInterface{
 	static kind = 1
 	slots = [null, null, null, null, null, null, null, null, null]
 	output = null; canProduce = 0
-	getItem(id, slot){ return id == 1 ? this.output : id == 0 && slot < 9 ? this.slots[slot] : null }
+	getItem(id, slot){ return slot == 9 ? this.output : slot < 9 ? this.slots[slot] : null }
 	setItem(id, slot, item){
-		if(id == 1) this.output = item
+		if(slot == 9) this.output = item
 		else if(slot < 9) this.slots[slot] = item
 		this.calculateOutput()
 	}
 	calculateOutput(){
 		this.output = null; this.canProduce = 0
-		if(this.slots[4]?.id === Items.oak_log.id) this.output = new Items.oak_planks(4), this.canProduce = this.slots[4].count
-		this.itemChanged(1, 0)
+		
+		this.itemChanged(0, 9, this.output)
 	}
 	putItems(id, slot, item){
-		if(id == 0) return super.putItems(id, slot, item)
+		if(slot < 9) return super.putItems(id, slot, item)
 		else return item
 	}
 	takeItems(id, slot, count){
-		if(id == 0) return super.takeItems(id, slot, count)
+		if(slot < 9) return super.takeItems(id, slot, count)
 		else if(this.output){
 			const count = min(floor(this.output.maxStack/this.output.count), this.canProduce)
 			const s = new this.output.constructor(count*this.output.count)
 			let changed = false
-			for(let i = 0; i < 9; i++) if(this.slots[i] && (this.slots[i].count-=count)<=0) this.slots[i] = null, changed = true
+			for(let i = 0; i < 9; i++){
+				if(!this.slots[i]) continue
+				if((this.slots[i].count-=count)<=0) this.slots[i] = null, changed = true
+				this.itemChanged(0, i, this.slots[i])
+			}
 			if(changed) this.calculateOutput()
 			return s
 		}
 	}
 	swapItems(id, slot, item){
-		if(id == 0) return super.swapItems(id, slot, item)
+		if(slot < 9) return super.swapItems(id, slot, item)
 		else return item
 	}
 	interfaceClosed(id, player){
-		if(id == 0){
-			for(let i = 0; i < 9; i++){
-				const itm = this.slots[i]
-				if(!itm) continue
-				player.giveAndDrop(itm)
-				this.itemChanged(0, i, this.slots[i] = null)
-			}
+		for(let i = 0; i < 9; i++){
+			const itm = this.slots[i]
+			if(!itm) continue
+			player.giveAndDrop(itm)
+			this.itemChanged(0, i, this.slots[i] = null)
 		}
 	}
 }
