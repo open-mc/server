2b5e1b75f823ff036be6d5108d38dbc6400ee949
BlobTheKat <blob.kat@hotmail.com>
2024-05-30 13:46:54 +0100

    glowstone

--- blocks/building/glass.js
+++ blocks/building/glass.js
@@ -5,3 +5,8 @@ Blocks.glass = class extends Block{
 	static breaktime = 0.6
 	drops(){ return new Items.glass() }
 }
+
+Blocks.glowstone = class extends Blocks.glass{
+	static breaktime = 0.45
+	drops(){ return new Items.glowstone() }
+}
--- items/building/glass.js
+++ items/building/glass.js
@@ -1,5 +1,7 @@
-import { Item, Items } from '../item.js'
+import { Items } from '../item.js'
 import { Blocks } from '../../blocks/block.js'
 import { itemify } from '../../blocks/blockshapes.js'
 
 Items.glass = itemify(Blocks.glass)
+
+Items.glowstone = itemify(Blocks.glowstone)
