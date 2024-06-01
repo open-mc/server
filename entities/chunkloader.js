c96ee170db7f494f73d19b6412497b3c60f08fe8
BlobTheKat <blob.kat@hotmail.com>
2024-06-01 17:24:58 +0100

    radius

--- entities/chunkloader.js
+++ entities/chunkloader.js
@@ -1,7 +1,7 @@
 import { DataWriter } from '../modules/dataproto.js'
 
 export const ChunkLoader = T => class extends T{
-	radius = CONFIG.world.chunk_loading_range
+	radius = max(2, CONFIG.world.chunk_loading_range+1)
 	moved(){
 		const {world, _world, radius, sock} = this
 		if((!_world && !world) || !sock) return
