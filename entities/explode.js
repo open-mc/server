39e08a68fc6befb7ae1fb77db031af5629ee1972
BlobTheKat <blob.kat@hotmail.com>
2024-09-09 01:36:10 +0100

    d

--- entities/explode.js
+++ entities/explode.js
@@ -77,7 +77,6 @@ export function explode(entity, strength = 100, fire = false){
 	jump(-x,-y); x = getX(); y = getY()
 	select(-LEFT, -LEFT, LEFT, LEFT, e => {
 		let dx = e.x - x, dy = e.y - y
-		console.log(dy)
 		const dmg = buffer[(floor(dx+LEFT)|0) + (floor(dy+LEFT)|0)*DIAMETER]
 		const d = sqrt(dx * dx + dy * dy)
 		dx /= d; dy /= d
