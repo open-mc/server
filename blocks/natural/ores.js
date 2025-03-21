0d8cc307d3b0387a2d7a0fa699ef4dae28240d81
BlobTheKat <blob.kat@hotmail.com>
2025-03-21 09:49:02 +0000

    other ores

--- blocks/natural/ores.js
+++ blocks/natural/ores.js
@@ -1,18 +1,36 @@
 import { Items } from '../../items/item.js'
 import { Block, Blocks } from '../block.js'
 
-Blocks.coal_ore = class CoalOre extends Block{
+Blocks.coal_ore = class extends Block{
 	static tool = 'pick'
 	static breaktime = 15
 	drops(){ return new Items.coal() }
 }
 
-Blocks.iron_ore = class IronOre extends Block{
+Blocks.iron_ore = class extends Block{
 	static tool = 'pick'
 	static breaktime = 15
 	drops(){ return new Items.iron() }
 }
 
-Blocks.quartz_ore = class QuartzOre extends Block{
+Blocks.gold_ore = class extends Block{
+	static tool = 'pick'
+	static breaktime = 15
+	drops(){ return new Items.gold() }
+}
+
+Blocks.diamond_ore = class extends Block{
+	static tool = 'pick'
+	static breaktime = 15
+	drops(){ return new Items.diamond() }
+}
+
+Blocks.lapis_ore = class extends Block{
+	static tool = 'pick'
+	static breaktime = 15
+	drops(){ return new Items.lapis() }
+}
+
+Blocks.quartz_ore = class extends Block{
 	static tool = 'pick'
 }
--- worldgen/features/orevein.js
+++ worldgen/features/orevein.js
@@ -2,17 +2,37 @@ import { Blocks, Features } from "../globals.js"
 import { getY } from "../util/chunk.js"
 import { Vein } from "../util/vein.js"
 
-export const CoalVein = Features.coal_ore = v => {
+Features.coal_ore = v => {
 	if(v > .75) return false
 	const v1 = v%.25
 	const th = 8*PI*v1, r = (v-v1)*4+5
 	Vein(2.25, Blocks.coal_ore, Blocks.stone)(1.25, sin(th)*r, cos(th)*r)
 }
 
-export const IronVein = Features.iron_ore = v => {
-	const prob = .45+max(0, 128-abs((getY()+192&511)-128))*.004
+Features.iron_ore = v => {
+	const prob = min(.9,.45+max(0, 128-abs((getY()+192&511)-128))*.005)
 	if(v > prob) return false
 	Vein(v+1.4, Blocks.iron_ore, Blocks.stone)
 }
 
-Features.ores = [Features.coal_ore, Features.iron_ore]
+Features.gold_ore = v => {
+	const prob = max(0, 128-abs((getY()-128&511)-128))*.005
+	if(v > prob) return false
+	Vein(v+1.2, Blocks.gold_ore, Blocks.stone)
+}
+
+Features.diamond_ore = v => {
+	const y = getY()
+	if(y > -64) return false
+	const prob = y > -320 ? (-64-y)*.0009765625 : y > -1600 ? .25 : y > -3200 ? y*-.00015625 : .5
+	if(v > prob) return false
+	Vein(v+1, Blocks.diamond_ore, Blocks.stone)
+}
+
+Features.lapis_ore = v => {
+	const prob = min(.9,.45+max(0, 128-abs((getY()&511)-128))*.004)
+	if(v > prob) return false
+	Vein(1, Blocks.lapis_ore, Blocks.stone)
+}
+
+Features.ores = [Features.coal_ore, Features.iron_ore, Features.gold_ore, Features.diamond_ore, Features.lapis_ore]
